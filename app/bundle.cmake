446a3595d41d752b27e4c7be73956e01d9fe8463
Fulva1230 <b10503137@gapps.ntust.edu.tw>
2023-03-17 20:39:44 +0800

    bundling and deploying

--- app/bundle.cmake
+++ app/bundle.cmake
@@ -1,13 +1,14 @@
 file(GET_RUNTIME_DEPENDENCIES 
 RESOLVED_DEPENDENCIES_VAR resolved_deps
 UNRESOLVED_DEPENDENCIES_VAR unresolved_deps
-DIRECTORIES "${search_path}"
-POST_INCLUDE_REGEXES "${search_path}"
+DIRECTORIES ${search_path}
+POST_INCLUDE_REGEXES ${search_path}
 POST_EXCLUDE_REGEXES ".*"
 EXECUTABLES ${target_exe}
 )
 
-file(COPY ${resolved_deps} DESTINATION ".")
+file(COPY ${target_exe} DESTINATION dist)
+file(COPY ${resolved_deps} DESTINATION dist)
 
 message(resolved: ${resolved_deps})
 message(unresolved: ${unresolved_deps})
